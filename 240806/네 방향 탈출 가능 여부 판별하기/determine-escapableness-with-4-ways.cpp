8048980c338742f3b6e6b08d53b1f62d577be360
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-06 15:32:31 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+void bfs();
+
+int vit[100][100] = {0};
+int arr[100][100] = {0};
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0, -1, 1}; 
+int n, m;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    cin >> n >> m;
+
+    int a,b;
+    for(int i = 0; i < n;i++)
+        for(int j = 0; j < m;j++)
+            cin >> arr[i][j];
+    
+    bfs();
+}
+void bfs(){
+    queue<pair<int,int>> q;
+    q.push({0,0});
+    int ans = 0;
+
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second; 
+        q.pop();
+        vit[x][y] = true;
+
+        if(x == n-1 && y == m-1){
+            ans = 1;
+            break;
+        }
+        for(int i = 0; i< 4;i++){
+            int nx = x + dx[i], ny = y + dy[i];
+
+            if(nx < 0 || nx>= n || ny < 0 || ny >= n || arr[nx][ny] == 0 || vit[nx][ny] == 1)
+                continue;
+
+            q.push({nx,ny});
+            vit[nx][ny] = 1;
+        }
+    }
+    cout << ans;
+}
