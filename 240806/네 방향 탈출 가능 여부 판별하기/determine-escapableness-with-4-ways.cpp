344682ac1495af5e89169ebb0395801945b874d4
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-06 15:35:02 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 240806/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -50,7 +50,7 @@ void bfs(){
         for(int i = 0; i< 4;i++){
             int nx = x + dx[i], ny = y + dy[i];
 
-            if(nx < 0 || nx>= n || ny < 0 || ny >= n || arr[nx][ny] == 0 || vit[nx][ny] == 1)
+            if(nx < 0 || nx>= n || ny < 0 || ny >= m || arr[nx][ny] == 0 || vit[nx][ny] == 1)
                 continue;
 
             q.push({nx,ny});
