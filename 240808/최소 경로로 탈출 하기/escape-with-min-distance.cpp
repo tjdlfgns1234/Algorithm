c336452dbddb908cb43649e0b52f7ce2e176b89d
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-08 10:45:05 +0900

    Title: 최소 경로로 탈출 하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/최소 경로로 탈출 하기/escape-with-min-distance.cpp
+++ 240808/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -11,6 +11,7 @@ int dy[4] = {0,0, 1,-1};
 
 int arr[100][100] = {0};
 int chk[100][100] = {0};
+int ans[100][100] = {0};
 
 int main()
 {
@@ -33,31 +34,27 @@ void solve() {
 }
 void bfs(){
 
-    queue <pair<int,int>> q,q2;
-    q2.push({0,0}), chk[0][0] = 1;
+    queue <pair<int,int>> q;
+    q.push({0,0}), chk[0][0] = 1;
     int curr = 0;
-    while(!q2.empty()){
-        q = q2;
-        while(!q2.empty())
-            q2.pop();
-        while(!q.empty()){
-            int x = q.front().first, y = q.front().second;
-            q.pop();
-            for(int i = 0; i< 4;i++){
-                int nx = x + dx[i], ny = y + dy[i];
 
-                if(nx < 0 || nx >= n || ny < 0|| ny >= m)
-                    continue;
+    while(!q.empty()){
+        int x = q.front().first, y = q.front().second;
+        q.pop();
+        for(int i = 0; i< 4;i++){
+            int nx = x + dx[i], ny = y + dy[i];
 
-                if(chk[nx][ny] == 0 && arr[nx][ny] == 1)
-                    chk[nx][ny] = 1, q2.push({nx,ny}); 
-            }
-            if(chk[n-1][m-1] == 1){
-                cout << curr +1 << '\n';
-                return;
-            }
+            if(nx < 0 || nx >= n || ny < 0|| ny >= m)
+                 continue;
+
+            if(chk[nx][ny] == 0 && arr[nx][ny] == 1)
+                chk[nx][ny] = 1,ans[nx][ny] = ans[x][y] + 1, q.push({nx,ny}); 
         }
-        curr++;
+        if(chk[n-1][m-1] == 1){
+            cout << ans[n-1][m-1] << '\n';
+            return;
+        }
     }
+
     cout << -1;
 }
