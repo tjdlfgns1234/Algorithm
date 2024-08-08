36c07100177881943aa9afd285445a7ae610c2f8
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-08 10:35:45 +0900

    Title: 최소 경로로 탈출 하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+void bfs();
+
+int n,m;
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0, 1,-1};
+
+int arr[100][100] = {0};
+int chk[100][100] = {0};
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    cin >> n >> m;
+
+    for(int i = 0; i < n;i++)
+        for(int j = 0; j < n;j++)
+            cin >> arr[i][j];
+        
+
+    bfs();
+}
+void bfs(){
+
+    queue <pair<int,int>> q,q2;
+    q2.push({0,0}), chk[0][0] = 1;
+    int ans = 0, curr = 0;
+    while(!q2.empty()){
+        q = q2;
+        while(!q2.empty())
+            q2.pop();
+        while(!q.empty()){
+            int x = q.front().first, y = q.front().second;
+            q.pop();
+            for(int i = 0; i< 4;i++){
+                int nx = x + dx[i], ny = y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0|| ny >= m)
+                    continue;
+
+                if(chk[nx][ny] == 0 && arr[nx][ny] == 1)
+                    chk[nx][ny] = 1, q2.push({nx,ny}); 
+            }
+        }
+        curr++;
+    }
+
+    if(chk[n-1][m-1] != 0)
+        cout << curr-1;
+    else
+        cout << -1;
+
+
+}
