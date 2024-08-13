b0e3545b86fc085715b90dd45eed0061dbf15a97
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-13 10:10:04 +0900

    Title: 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240813/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+void calc();
+
+int n, m;
+int arr[100];
+int dp[10001] = {0};
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve() {
+    cin >> n >> m;
+
+    for(int i = 0; i < n;i++)
+        cin >> arr[i];
+    
+    calc();
+}
+void calc(){
+    for(int i = 0; i < n;i++)
+        for(int j = 1; j<=m;j++)
+            if(j-arr[i] >= 0)
+                dp[j] = min(dp[j-arr[i]]+1,dp[j]+1); 
+            
+    cout << dp[m];
+}
