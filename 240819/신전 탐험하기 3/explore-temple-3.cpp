345a39808750b609b2ad379e9678e36e02992bbe
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-19 15:35:51 +0900

    Title: 신전 탐험하기 3, Time: 13ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240819/신전 탐험하기 3/explore-temple-3.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+void calc();
+void init();
+
+int n, m;
+int dp[1001][101] = {0};
+int arr[1001][101] = {0};	
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void init(){
+    for(int i = 1; i <= n; i ++)
+        for(int j = 1; j <= m; j++)
+            dp[i][j] = 0;
+}
+void solve() {
+    cin >> n >> m;
+
+    for(int i = 1; i <= n;i++)
+        for(int j = 1; j <= m;j++)
+            cin >> arr[i][j];
+
+    calc();
+}
+void calc(){
+
+    init();
+    // 1계단 올라가는 경우는 최대 3번
+
+    for(int i =1; i <= m;i++)
+        dp[1][i] = arr[1][i];
+
+    for(int i = 2; i <= n; i++) 
+        for(int j = 1; j <=m ; j++) 
+            for(int k = 1; k <= m ;k++)
+                if(j == k) continue;
+                else dp[i][j] = max(dp[i-1][j] + arr[i][k], dp[i][j]);
+    
+    
+    int ans = INT_MIN;
+    for (int i = 1; i <= m; i++)
+        ans = max(ans, dp[n][i]);
+    cout << ans;
+}
