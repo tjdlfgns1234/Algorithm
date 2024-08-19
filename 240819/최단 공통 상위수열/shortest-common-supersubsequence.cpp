512317f0168fa61f4517cf2ab0f2d0a8ccda2f14
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-08-19 16:12:13 +0900

    Title: 최단 공통 상위수열, Time: 11ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240819/최단 공통 상위수열/shortest-common-supersubsequence.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+void calc();
+void init();
+
+string a,b;
+int dp[1001][1001] = {0};
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL), std::cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void init(){
+    for(int i = 1; i <= a.size(); i ++)
+        for(int j = 1; j <= b.size(); j++)
+            dp[i][j] = 0;
+}
+void solve() {
+    cin >> a >> b;
+
+    calc();
+}
+void calc(){
+
+    init();
+    // 1계단 올라가는 경우는 최대 3번
+    int size = a.size();
+    for(int i =1; i <= a.size();i++)
+        dp[1][i] = 1;
+
+    for(int i = 1; i <= a.size(); i++) 
+        for(int j = 1; j <=b.size() ; j++) 
+            if(a[i-1]== b[j-1])
+                dp[i][j] = dp[i-1][j-1]+1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+
+
+    // for (int i = 1; i <= size; i++)
+    //     cout <<  dp[size][i] << ' ';
+    
+   cout << a.size() + b.size() - dp[a.size()][b.size()];
+}
