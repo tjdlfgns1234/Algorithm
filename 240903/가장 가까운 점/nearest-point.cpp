2b2a44ddba67a82bb34675ab43f9ba3e80a408ab
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-03 12:32:12 +0900

    Title: 가장 가까운 점, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240903/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+
+struct p{
+    int x,y;
+};
+
+bool operator <(const p& a,const p& b){
+    return ((a.x + a.y)>(b.x + b.y));
+}
+
+int main() {
+    cin >> n >> m;
+
+    priority_queue <p> pq;
+
+    p tmp;
+    while(n--){
+        cin >> tmp.x >> tmp.y;
+        pq.push(tmp);
+    }
+
+    while(m--){
+        tmp = pq.top();
+        tmp.x += 2, tmp.y += 2;
+        pq.pop();
+        pq.push(tmp);
+    }
+
+    cout << pq.top().x << ' ' << pq.top().y;
+
+    return 0;
+}
