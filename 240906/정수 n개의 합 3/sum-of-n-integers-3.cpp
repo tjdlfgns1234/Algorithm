e8ffd4e145cbad3aa5c3549d90bec4ff449f9f00
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-06 17:37:09 +0900

    Title: 정수 n개의 합 3, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240906/정수 n개의 합 3/sum-of-n-integers-3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,k;
+int arr[101][101];
+int dp[101][101];
+
+int main() {
+    cin >> n >> k;
+
+    for(int i = 1; i <=n;i++)
+        for(int j = 1; j <= n; j++)
+            cin >> arr[i][j];
+
+    for(int i = 1; i <=n;i++)
+        for(int j = 1; j <= n; j++)
+            dp[i][j] = dp[i-1][j] +
+                      dp[i][j-1] -
+                      dp[i-1][j-1] +
+                      arr[i][j];
+    int ans = 0;
+
+    for(int i = 1; i <= n;i++)
+        for(int j = 1; j <= n; j++)
+            if(i-k >=0 && j - k>= 0)
+                ans = max(ans, dp[i][j] - dp[i-k][j] - dp[i][j-k] + dp[i-k][j-k]);
+
+    cout << ans;
+
+    return 0;
+}
