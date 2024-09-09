76d61f1bbf374ed05706a2c0defa8137abff98c9
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-09 14:46:16 +0900

    Title: 돌의 소속, Time: 97ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240909/돌의 소속/belonging-to-a-rock.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+
+int n,k;
+int arr[100001] = {0};
+int dp[3][100001] = {0};
+
+
+int main() {
+
+    ios::sync_with_stdio(NULL);
+    cout.tie(NULL), cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve(){
+    cin >> n >> k;
+
+    for(int i = 1; i <= n;i++)
+        cin >> arr[i];
+    
+    for(int i = 1; i <= n;i++){
+        int stone = arr[i]-1;
+
+        dp[0][i] = dp[0][i-1];
+        dp[1][i] = dp[1][i-1];
+        dp[2][i] = dp[2][i-1];
+        dp[stone][i]++; // 돌 증가
+    }
+
+
+    int a,b;
+    for(int i = 0; i < k;i++){
+        cin >> a >> b;
+
+        cout << dp[0][b] - dp[0][a-1] << " ";
+        cout << dp[1][b] - dp[1][a-1] << " ";
+        cout << dp[2][b] - dp[2][a-1] << "\n";
+    }
+
+
+
+
+}
