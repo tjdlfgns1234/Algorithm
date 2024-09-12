40410ae3cb2edca9dc28a4012228127f96b14419
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-12 11:51:29 +0900

    Title: 점 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240912/점 개수 세기/count-number-of-points.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,q;
+
+map <int,int> mp;
+int dp[100001] = {0};
+
+int main() {
+    cin >> n >> q;
+
+    int tmp, idx = 0;
+    for(int i = 0; i < n;i++) cin >> tmp, mp[tmp]++;
+
+    idx = 1;
+    for(auto &i : mp) i.second = idx++;
+
+    int a,b;
+    for(int i =0; i < q;i++){
+        cin >> a >> b;
+
+        auto q = mp.lower_bound(a),w = mp.upper_bound(b);
+        if(q == mp.begin()&& w != mp.end())
+            cout << w->second -1 << '\n';
+        else if(q == mp.begin()&& w == mp.end())
+            cout << n << '\n';
+        else if(q != mp.begin()&& w != mp.end())
+            cout << w->second - q->second << '\n';
+        else
+            cout << n - q->second << '\n';
+
+    }
+
+    return 0;
+}
