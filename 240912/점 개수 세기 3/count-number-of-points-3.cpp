cadb5f84a7c234e9def3e506a4b0c37c0ba4f496
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-12 10:58:37 +0900

    Title: 점 개수 세기 3, Time: 344ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240912/점 개수 세기 3/count-number-of-points-3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,q;
+
+map <int,int> mp;
+
+
+int main() {
+    cin >> n >> q;
+
+    int tmp, idx = 0;
+    for(int i = 0; i < n;i++) cin >> tmp, mp[tmp] = idx++;
+
+    idx = 1;
+    for(auto i : mp)i.second = idx++;
+
+    int a,b;
+    for(int i =0; i < q;i++){
+        cin >> a >> b;
+        cout << mp[b] - mp[a] +1 << '\n';
+    }
+
+
+
+    return 0;
+}
