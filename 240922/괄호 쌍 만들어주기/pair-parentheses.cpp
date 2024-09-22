d47518b05f3c04e28d2db5651d2f3015af8f30b1
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-22 16:07:03 +0900

    Title: 괄호 쌍 만들어주기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240922/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+
+int pre[100002] = {0};
+int pre2[100002] = {0};
+
+int main() {
+ 
+    solve();
+
+    return 0;
+}
+void solve(){
+    string s;
+    cin >> s;
+
+    int idx = s.size()-2;
+    for(int i = s.size()-1; i > 0 && idx >= 0; i--){
+        if(s[idx] == ')'&& s[idx+1] == ')')
+            pre[i] = pre[i+1] + 1;
+        else
+            pre[i]= pre[i+1];
+        idx--;
+    }
+    
+    int ans = 0;
+    for(int i = 1; i < s.size();i++)
+        if(s[i-1] == s[i] && s[i] == '(')
+            ans += pre[i];
+    
+    // for(int i = 1; i < s.size();i++)
+    //     cout << pre[i] << ' ' ;
+
+    cout << ans << '\n';
+}
