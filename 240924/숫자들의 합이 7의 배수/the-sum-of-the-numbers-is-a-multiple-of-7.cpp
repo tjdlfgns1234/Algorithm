b109587b4b311596b534000f28425c366cab4b72
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-24 09:31:03 +0900

    Title: 숫자들의 합이 7의 배수, Time: 1004ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240924/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.cpp
+++ 240924/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.cpp
@@ -14,21 +14,22 @@ int main() {
 void solve(){
     cin >> n;
 
-    vector<long long> arr(n);
+    vector<int> arr(n);
 
     for(auto&i : arr) cin >>i;
 
     // 연속하게 고른 숫자들의 합이 7의 배수 (x%7==0)
     // 숫자는 서로 다름
 
-    long long ans = 0;
-    long long sum = 0;
-    for(ll i = 0; i < n;i++){
+    int ans = 0;
+    int sum = 0;
+    for(int i = 0; i < n;i++){
         sum=0;
-        for(ll j = i; j < n;j++){
+        for(int j = i; j < n;j++){
             sum += arr[j];
             if(sum%7==0)
                 ans = max(ans,j-i+1);
+            sum = sum%7;
         }
     }
 
