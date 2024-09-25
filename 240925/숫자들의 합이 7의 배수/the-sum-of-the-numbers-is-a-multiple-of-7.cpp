5959851b22b6a9411325cbe0a239b7dd7ec65bc7
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-09-25 14:00:26 +0900

    Title: 숫자들의 합이 7의 배수, Time: 14ms, Memory: 1MB, Status: Passed - CodeTree

--- 240925/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.cpp
+++ 240925/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int n;
 ll arr[50001] = {0};
-ll dp[50001] = {0};
+ll pre[50001] = {0};
+
+vector<vector<ll>> chk(7);
 
 void solve();
 
@@ -19,7 +21,7 @@ int main() {
 void solve(){
     cin >> n;
 
-    for(int i = 0; i < n;++i) cin >> arr[i];
+    for(int i = 1; i <= n;++i) cin >> arr[i];
 
     // 연속하게 고른 숫자들의 합
     // 7의 배수 (x%7==0)
@@ -27,17 +29,18 @@ void solve(){
 
     int ans = 0,sum = 0;
 
-    dp[0] = arr[0];
-    for(int i = 1; i < n;++i) dp[i] += arr[i]; // prefix 저장
+    for(int i = 1; i <= n;++i) pre[i] =(pre[i-1] + arr[i])%7; // prefix 저장
 
-    for(int i = 0; i < n;++i){
-        sum=0;
-        for(int j = i; j < n;++j){
-            sum =(sum + arr[j]) %7;
-            if(sum == 0)
-                ans = max(ans,j-i+1);
-            if(n-j+1 < ans)
-                break;
+    for(int i = 0; i <= n;++i)
+        chk[pre[i]].push_back(i);
+
+    // 나머지 값이 같은 가장 멀리 떨어진 두 인덱스 차이를 계산
+    for (int i = 0; i < 7; i++) {
+        if (chk[i].size() >= 2) {
+            // 가장 작은 인덱스와 가장 큰 인덱스의 차이 계산
+            int b = *max_element(chk[i].begin(),chk[i].end());
+            int s = *min_element(chk[i].begin(),chk[i].end());
+            ans = max(ans, b-s);
         }
     }
 
