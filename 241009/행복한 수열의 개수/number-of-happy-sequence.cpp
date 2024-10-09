c5f0594ca5ad4676f760e7e56f8a371d1db43abc
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-10-09 21:52:00 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 241009/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,72 +1,58 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-//격자 크기는 1 이상 100 이하
-//행복한 수열: 연속해서 m개 이상의 동일한 원소가 나오는 순간이 존재하는 수열
-//그냥 1부터 n배열의 행과 열을 각각 체크해서 행복한 수열이 존재하는지 확인하면 되는거 아닌가?
-void happy_arr();
-int m, n, ans, arr[101][101];
-//그러면 그냥 
+int n,m;
+int arr[101][101] = {0};
+
+void solve();
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     cin >> n >> m;
-    
-    //수열을 입력받음
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >>arr[i][j];
-        }
-    }
 
-    if(m == 1){
-        ans = 2*n;
-    }
-    else{
-        happy_arr();
-    }
+    for(int i = 0; i < n;i++)
+        for(int j = 0; j < n;j++)
+            cin >> arr[i][j];
 
-    cout << ans <<'\n';
+    solve();
 
     return 0;
 }
-
-void happy_arr(){
-
-    //어떤 숫자든m개만 있으면 됨 (ex)m = 2 , 22, 11, 44, 55....)
-    //그럼 숫자를 하나하나 체크해서 for문 돌려서 같은 숫자면 세는걸로 하자
-    for(int row = 0; row<n; row++){ 
-        int cnt = 0;
-        int pre = 0;
-        for(int col=0; col<n; col++){
-            pre = arr[row][col]; //이전 값 저장
-            //이전값과 현재 값이 같다면
-            if(pre == arr[row][col]){
+void solve(){
+    // m개 이상의 동일한 원소
+    int ans = 0;
+    for(int i = 0; i < n;i++){
+        int pre = -1, cnt = 0;
+        for(int j = 0; j < n;j++){
+            if(cnt == 0){
+                pre = arr[i][j];
+                cnt = 1;
+            }
+            else if(pre == arr[i][j])
                 cnt++;
+            else
+                cnt = 1, pre = arr[i][j];
+            if(cnt == m){
+                ans++;
+                break;
             }
         }
-        if(cnt >= (m-1)){
-            ans++;
-            break;
-        }
-
-    }
-    for(int col = 0; col <n; col++){
-        int cnt = 0;
-        int pre = 0;
-        for(int row =0; row<n; row++){
-            pre = arr[col][0];
-            //이전값과 현재값이 같다면
-            if(pre == arr[col][row]){
+        pre = -1, cnt = 0;
+        for(int j = 0; j < n;j++){
+            if(cnt == 0){
+               pre = arr[j][i];
+               cnt = 1;
+            }
+            else if(pre == arr[j][i])
                 cnt++;
+            else
+                cnt = 1, pre = arr[j][i];
+    
+            if(cnt == m){
+                ans++;
+                break;
             }
         }
-        if(cnt >= (m-1)){
-            ans++;
-            break;
-        }
     }
+
+    cout << ans;
 }
