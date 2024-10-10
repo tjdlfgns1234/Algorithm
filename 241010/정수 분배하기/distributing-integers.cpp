863da29d7f61e91d326b839e4a1b33461954c08a
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-10-10 10:12:06 +0900

    Title: 정수 분배하기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241010/정수 분배하기/distributing-integers.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[100001] = {0};
+void solve();
+bool chk(int mid);
+
+int main() {
+    
+    solve();
+
+    return 0;
+}
+void solve(){
+    cin >> n >> m;
+
+    for(int i =0; i < n;i++) cin >> arr[i];
+
+    int l = 0, r = 1e5, ans = 0;
+    while(l<=r){
+        int mid = l + (r-l)/2;
+
+        if(chk(mid)) // 최댓값 찾는 문제
+            ans = max(ans,mid), l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    cout << ans;
+
+
+}
+bool chk(int mid){
+
+    int cnt = 0;
+
+    for(int i =0; i < n;i++)
+        cnt += arr[i]/mid;
+    
+    if(cnt >= m)
+        return true;
+    else
+        return false;
+}
