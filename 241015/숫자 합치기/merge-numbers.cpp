a1905e79fb8b1d06f4552a5008640d83d11f3979
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-10-15 10:00:50 +0900

    Title: 숫자 합치기, Time: 58ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241015/숫자 합치기/merge-numbers.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int n;
+void solve();
+
+int main() {
+    
+    solve();
+
+    return 0;
+}
+void solve(){
+    cin >> n;
+
+    ll tmp;
+    priority_queue<ll> q;
+    for(int i = 0; i < n; i++) cin >> tmp, q.push(-tmp);
+
+    ll ans = 0;
+    while(!q.empty() && q.size()>= 2){
+        ll x = -q.top(); q.pop();
+        ll y = -q.top(); q.pop();
+        ans += x + y;
+        q.push(-(x+y));
+    }
+
+    cout << ans;
+}
