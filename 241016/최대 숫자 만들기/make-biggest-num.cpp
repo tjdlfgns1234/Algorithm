5933b9b515b3451c0911b969d680826403b1a6b8
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-10-16 11:04:29 +0900

    Title: 최대 숫자 만들기, Time: 137ms, Memory: 2MB, Status: Passed - CodeTree

--- 241016/최대 숫자 만들기/make-biggest-num.cpp
+++ 241016/최대 숫자 만들기/make-biggest-num.cpp
@@ -5,10 +5,7 @@ int n;
 string s[50001];
 
 bool cmp(string a, string b) {
-    if(a+b > b+a)
-        return true;
-    else
-        return false;
+    return a+b > b+a;
 }
 void solve();
 
@@ -20,7 +17,7 @@ int main() {
 }
 void solve(){
     cin >> n;
-
+    // 한 숫자가 다른 숫자의 접두사가 되는 경우 조심
     for(int i = 0; i < n;i++) cin >> s[i];
 
     sort(s,s+n, cmp);
