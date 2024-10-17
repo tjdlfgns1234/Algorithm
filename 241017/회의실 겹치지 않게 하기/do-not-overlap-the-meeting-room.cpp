8302058e2a1c8d64770bf26bd149425aae77c38f
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-10-17 09:35:52 +0900

    Title: 회의실 겹치지 않게 하기, Time: 1043ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241017/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+struct p{
+    int s,e;
+};
+
+p arr[100001];
+
+bool cmp(p& a, p& b){
+    if(a.e < b.e)
+        return a.e < b.e;
+    return ((a.e-a.s) < (b.e - b.s));
+}
+void solve();
+
+int main() {
+
+    solve();
+
+    return 0;
+}
+void solve(){
+    cin >> n;
+
+    for(int i = 0; i < n;i++) cin >> arr[i].s >> arr[i].e;
+
+    sort(arr, arr+n,cmp);
+
+    int curr = arr[0].e, ans = 0;
+    for(int i = 1; i < n;i++){
+        // cout << arr[i].s << ' ' << arr[i].e << " " << curr << '\n';
+
+        if(curr <= arr[i].s)
+            curr = arr[i].e;
+        else
+            ans++;
+    }
+
+    cout << ans;
+}
