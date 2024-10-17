dd1543621d71db8626a0ab5c9ad1b8d0434c874b
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-10-17 10:31:19 +0900

    Title: G & H 반전시키기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241017/G & H 반전시키기/reversing-g-and-h.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+
+int main() {
+    
+    solve();
+    
+    return 0;
+}
+void solve(){
+    // 길이가 4인 문자열 0100
+    int n;
+    cin >> n;
+
+    string a,b;
+    cin >> a >> b;
+
+    int ans = 0, f = -1;
+    for(int i = 0; i < n;i++){
+        if(a[i] != b[i]){
+            if(f == -1)
+                ans++;
+            if(!f)
+                ans++;
+            f = 1;
+        }
+        else{
+            f = 0;
+        }
+    }
+    cout << ans;
+}
