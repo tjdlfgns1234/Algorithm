40e54af08e2b2d3c63c0f972bc5de95d0671cbe4
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-11-04 12:23:16 +0900

    Title: 최단 왕복, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/최단 왕복/shortest-round-trip.cpp
+++ 241104/최단 왕복/shortest-round-trip.cpp
@@ -6,7 +6,6 @@ void solve();
 
 int n,m;
 int dist[SIZE][SIZE];
-int dist2[SIZE][SIZE];
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -29,37 +28,27 @@ void solve(){
 
      for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++)
-            dist[i][j] = (int)1e9, dist2[i][j] = (int)1e9;
+            dist[i][j] = (int)1e9;
+        dist[i][i] = 0;
     }
 
-    // 그래프를 인접행렬로 표현
     for(int i = 0; i < m; i++) {
         int x, y, z;
         tie(x, y, z) = edges[i];
         dist[x][y] = min(dist[x][y],z);
     }
     
-    for(int k = 1; k <= n; k++) // 확실하게 거쳐갈 정점을 1번부터 N번까지 순서대로 정의합니다.
-        for(int i = 1; i <= n; i++) // 고정된 k에 대해 모든 쌍 (i, j)를 살펴봅니다.
+    for(int k = 1; k <= n; k++) 
+        for(int i = 1; i <= n; i++) 
             for(int j = 1; j <= n; j++)
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 
 
-    for(int i = 0; i < m; i++) {
-        int x, y, z;
-        tie(x, y, z) = edges[i];
-        dist2[y][x] = min(dist2[y][x],z);
-    }
-    
-    for(int k = 1; k <= n; k++) // 확실하게 거쳐갈 정점을 1번부터 N번까지 순서대로 정의합니다.
-        for(int i = 1; i <= n; i++) // 고정된 k에 대해 모든 쌍 (i, j)를 살펴봅니다.
-            for(int j = 1; j <= n; j++)
-                dist2[i][j] = min(dist2[i][j], dist2[i][k] + dist2[k][j]);
-
     int ans = 1e9;
     for(int i = 1; i <= n; i++) 
         for(int j = 1; j <= n; j++) 
-            ans = min(ans, dist[i][j] + dist2[i][j]);
+            if(i!=j)
+                ans = min(ans, dist[i][j] + dist[j][i]);
 
     cout << ans;
 }
