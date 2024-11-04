fc2e2df4256356b19a76e18b59f86e6d5cdf0ddf
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-11-04 13:08:34 +0900

    Title: 연결리스트 1, Time: 101ms, Memory: 14MB, Status: Passed - CodeTree

--- 241104/연결리스트 1/linked-list1.cpp
+++ 241104/연결리스트 1/linked-list1.cpp
@@ -11,25 +11,78 @@ struct Node {
     Node(string data) : data(data), prev(nullptr), next(nullptr) {};
 };
 
-// 두 노드를 연결해줍니다.
+void solve();
+void connect(Node *s, Node *e);
+void insertNext(Node *target, Node *s);
+void insertPrev(Node *target, Node *s);
+void printNode(Node *target);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+void solve(){
+string s;
+    cin >> s;
+
+    // 시작 값
+    Node *cur = new Node(s);
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int op;
+        cin >> op;
+
+        if (op == 1) { // 앞에 삽입
+            string data;
+            cin >> data;
+            Node *target = new Node(data);
+            insertPrev(cur, target);
+        }
+
+        if (op == 2) { // 뒤에 삽입
+            string data;
+            cin >> data;
+            Node *target = new Node(data);
+
+            insertNext(cur, target);
+        }
+
+        if (op == 3) { // 한칸 앞으로
+            if (cur->prev != nullptr) cur = cur->prev;
+        }
+
+        if (op == 4) { // 한칸 뒤로
+            if (cur->next != nullptr) cur = cur->next;
+        }
+
+        // 매 연산이 진행될 때마다 cur의 값을 출력합니다.
+        printNode(cur);
+    }
+}
 void connect(Node *s, Node *e) {
+    // 연결
     if (nullptr != s) s->next = e;
     if (nullptr != e) e->prev = s;
 }
-
-// target 뒤에 s를 삽입합니다.
 void insertNext(Node *target, Node *s) {
+    // 특정 노드의 다음에 삽입
     connect(s, target->next);
     connect(target, s);
 }
 
-// target 앞에 s를 삽입합니다.
 void insertPrev(Node *target, Node *s) {
+    // 특정 노드의 이전에 삽입
     connect(target->prev, s);
     connect(s, target);
 }
 
-// target의 이전 노드, target, target의 다음 노드의 값을 출력합니다.
 void printNode(Node *target) {
     string n = "(Null)";
 
@@ -44,53 +97,4 @@ void printNode(Node *target) {
     // 다음 노드가 존재하지 않는다면, Null을 출력합니다.
     if (target->next == nullptr) cout << n << "\n";
     else cout << target->next->data << "\n";
-}
-
-int main() {
-    // 맨 처음 문자열을 입력 받습니다.
-
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
-
-    string s;
-    cin >> s;
-
-    // 맨 처음 존재하는 노드를 만듭니다.
-    Node *cur = new Node(s);
-
-    // 연산의 개수를 입력 받습니다.
-    int n;
-    cin >> n;
-
-    while (n--) {
-        int op;
-        cin >> op;
-
-        if (op == 1) {
-            string data;
-            cin >> data;
-            Node *target = new Node(data);
-            insertPrev(cur, target);
-        }
-
-        if (op == 2) {
-            string data;
-            cin >> data;
-            Node *target = new Node(data);
-
-            insertNext(cur, target);
-        }
-
-        if (op == 3) {
-            if (cur->prev != nullptr) cur = cur->prev;
-        }
-
-        if (op == 4) {
-            if (cur->next != nullptr) cur = cur->next;
-        }
-
-        // 매 연산이 진행될 때마다 cur의 값을 출력합니다.
-        printNode(cur);
-    }
 }
