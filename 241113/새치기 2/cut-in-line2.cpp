f9805b63ab08783b0505c80c2e4f23071e557396
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2024-11-13 13:52:18 +0900

    Title: 새치기 2, Time: 19ms, Memory: 2MB, Status: Passed - CodeTree

--- 241113/새치기 2/cut-in-line2.cpp
+++ 241113/새치기 2/cut-in-line2.cpp
@@ -1,126 +1,180 @@
+#include <iostream>
 #include <bits/stdc++.h>
-#define all(x) x.begin(),x.end()
 
 using namespace std;
 
-list <string> heads[11];
+#define MAX_N 100000
+#define MAX_M 10
 
-void insert(int idx, string name);
-void insert_prev(string a, string b);
-void insert_prev_multi(string a, string b, string c);
-void remove(string a);
-void print();
-void solve();
+// 사람들을 나타내는 노드입니다.
+struct Node {
+    // 사람의 번호를 나타냅니다.
+    string name;
+    Node *prev, *next;
 
-int n, m, q, cnt = 0;
+    Node(string name) : name(name), prev(nullptr), next(nullptr) {}
+};
 
-map<string, int> mp;
+// 사람들을 나타내는 노드들을 저장합니다.
+Node *nodes[MAX_N + 1];
+
+// 각 줄의 시작과 끝을 나타냅니다.
+Node *head[MAX_M + 1], *tail[MAX_M + 1];
+
+// 각 사람이 몇 번 줄에 서있는지 나타냅니다.
+int lineNum[MAX_N + 1];
+
+// 각 사람의 이름들을 번호로 바꿔줍니다.
+unordered_map<string, int> personId;
+
+// 두 사람을 연결합니다.
+void connect(Node *s, Node *e) {
+    if (nullptr != s) s->next = e;
+    if (nullptr != e) e->prev = s;
+}
+
+// i번 사람을 집에 보냅니다.
+void del(Node *i) {
+    // i번 사람이 어느 줄에 서있는지 확인합니다.
+    int l = lineNum[personId[i->name]];
+
+    // i번 사람이 어느 줄에도 서있지 않다면 아무것도 하지 않습니다.
+    if (l == 0) return;
+
+    // i번 사람이 줄의 시작이었다면 줄의 시작을 다음 사람으로 바꿉니다.
+    if (head[l] == i) head[l] = head[l]->next;
+    // i번 사람이 줄의 끝이었다면 줄의 끝을 이전 사람으로 바꿉니다.
+    if (tail[l] == i) tail[l] = tail[l]->prev;
+
+    // i번 사람을 줄에서 뺍니다.
+    // 원래 i번 사람의 이전 사람과 다음 사람을 연결합니다.
+    if (nullptr != i->prev) i->prev->next = i->next;
+    if (nullptr != i->next) i->next->prev = i->prev;
+
+    // i번 사람이 어느 줄에도 서있지 않다고 표시합니다.
+    lineNum[personId[i->name]] = 0;
+    i->next = i->prev = nullptr;
+}
+
+// a번 사람을 b번 사람 앞에 서게 합니다.
+void insertFront(Node *a, Node *b) {
+    // b번 사람이 어느 줄에 서있는지 확인합니다.
+    int lineNumB = lineNum[personId[b->name]];
+    // b번 사람이 해당 줄의 맨 앞이었다면, a번 사람을 줄의 맨 앞으로 만듭니다.
+    if (head[lineNumB] == b) head[lineNumB] = a;
+    // a번 사람을 해당 줄에서 뺍니다.
+    del(a);
+
+    // a번 사람을 b번 사람 앞에 서게 합니다.
+    connect(b->prev, a);
+    connect(a, b);
+
+    // a번 사람이 어느 줄에 서있는지 표시합니다.
+    lineNum[personId[a->name]] = lineNumB;
+}
+
+// a번 사람부터 b번 사람까지를 c번 사람 앞에 이동합니다.
+void move_partial_front(Node *a, Node *b, Node *c) {
+    // a, c번 사람이 어느 줄에 서있는지 확인합니다.
+    int lineNumA = lineNum[personId[a->name]];
+    int lineNumC = lineNum[personId[c->name]];
+
+    // a번 사람이 해당 줄의 맨 앞이었다면, 해당 줄의 맨 앞 사람을 b번 사람의 뒤로 변경합니다.
+    if (head[lineNumA] == a) head[lineNumA] = b->next;
+    // b번 사람이 해당 줄의 맨 끝이었다면, 해당 줄의 맨 끝 사람을 a번 사람의 앞으로 변경합니다.
+    if (tail[lineNumA] == b) tail[lineNumA] = a->prev;
+
+    // a번 사람부터 b번 사람까지를 줄에서 뺍니다.
+    // 이때 a번 사람의 이전 사람과 b번 사람의 다음 사람을 연결합니다.
+    connect(a->prev, b->next);
+
+    // c번 사람 앞에 a번 사람부터 b번 사람까지를 넣습니다.
+    // 이때 c번 사람이 해당 줄의 맨 앞이었다면, a번 사람을 줄의 맨 앞으로 만듭니다.
+    if (head[lineNumC] == c) {
+        connect(b, c);
+        head[lineNumC] = a;
+    }
+        // 아니라면, c번 사람의 이전 사람과 a번 사람을 연결합니다.
+        // 또한 b번 사람과 c번 사람을 연결합니다.
+    else {
+        connect(c->prev, a);
+        connect(b, c);
+    }
+
+    // a번 사람부터 b번 사람까지 어느 줄에 서 있는지 표시합니다.
+    Node *cur = a;
+    while (cur != b) {
+        lineNum[personId[cur->name]] = lineNumC;
+        cur = cur->next;
+    }
+    lineNum[personId[cur->name]] = lineNumC;
+}
+
+// 해당 줄을 전부 출력합니다.
+void printLine(int l) {
+    Node *cur = head[l];
+
+    if (cur == nullptr) {
+        cout << -1 << "\n";
+        return;
+    }
+
+    while (nullptr != cur) {
+        cout << cur->name << " ";
+        cur = cur->next;
+    }
+    cout << "\n";
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
-    solve();
-
-    return 0;
-}
-void solve() {
+    // 입력
+    int n, m, q;
     cin >> n >> m >> q;
 
-    string name;
-    for (int i = 1; i <= n; i++) {
-        cin >> name;
-        insert(i / (n / m) + (i % (n / m) != 0), name);
-    }
-
-    //print();
-
-    while (q--) {
-        int op;
-        cin >> op;
-
-        string a, b, c;
-
-        if (op == 1) {
-            cin >> a >> b;
-            insert_prev(a, b);
-        }
-        else if (op == 2) {
-            cin >> a;
-            remove(a);
-        }
-        else if (op == 3) {
-            cin >> a >> b >> c;
-            insert_prev_multi(a, b, c);
-        }
-        // cout << '\n';
-        // print();
-        // cout << '\n';
-        // print();
-    } 
-
-    print();
-}
-void insert(int idx, string name) {
-    heads[idx].push_back(name);
-    mp[name] = idx;
-}
-void print() {
+    // m 개의 줄을 입력 받습니다.
+    int personNum = 1;
     for (int i = 1; i <= m; i++) {
-        if (!heads[i].empty()) {
-            for (auto it = heads[i].begin(); it != heads[i].end(); it++)
-                cout << *it << " ";
-            cout << '\n';
+        for (int j = 0; j < n / m; j++) {
+            string t;
+            cin >> t;
+            personId[t] = personNum;
+            lineNum[personNum] = i;
+
+            if (j == 0) {
+                tail[i] = head[i] = nodes[personNum] = new Node(t);
+            } else {
+                nodes[personNum] = new Node(t);
+                connect(tail[i], nodes[personNum]);
+                tail[i] = nodes[personNum];
+            }
+
+            personNum++;
         }
-        else
-            cout << -1 << '\n';
     }
-    
-}
-void insert_prev(string a, string b) {
-    heads[mp[a]].erase(remove(all(heads[mp[a]]), a));
-    mp[a] = mp[b];
 
-    for(auto it = heads[mp[b]].begin(); it != heads[mp[b]].end(); it++)
-        if (*it == b) {
-            heads[mp[b]].insert(it, a);
-            break;
+    // q 개의 문자대로 시뮬레이션을 진행합니다.
+    while (q--) {
+        int option;
+        cin >> option;
+
+        if (option == 1) {
+            string x, y;
+            cin >> x >> y;
+            int a = personId[x], b = personId[y];
+            insertFront(nodes[a], nodes[b]);
+        } else if (option == 2) {
+            string x;
+            cin >> x;
+            int a = personId[x];
+            del(nodes[a]);
+        } else if (option == 3) {
+            string x, y, z;
+            cin >> x >> y >> z;
+            int a = personId[x], b = personId[y], c = personId[z];
+            move_partial_front(nodes[a], nodes[b], nodes[c]);
         }
-            
-}
-void remove(string a) {
-    for (auto it = heads[mp[a]].begin(); it != heads[mp[a]].end(); it++) 
-        if(*it == a){
-            heads[mp[a]].erase(it);
-            break;
-        }
-    mp[a] = -1;
-}
-void insert_prev_multi(string a, string b, string c) {
-    // a, b, c의 위치
-   
-    list <string> tmp;
+    }
 
-    bool f = false;
-    for (auto it = heads[mp[a]].begin(); it != heads[mp[a]].end(); it++) 
-        if (*it == b)
-            tmp.push_back(*it), f = false;
-        else if (*it == a)
-            tmp.push_back(*it), f = true;
-        else if (f)
-            tmp.push_back(*it);
-    
-    //print();
-
-    auto tmp_it = tmp.begin();
-    for (auto it = heads[mp[a]].begin(); it != heads[mp[a]].end(); it++) 
-        if (*it == *tmp_it) 
-             mp[*it] = mp[c], it = heads[mp[a]].erase(it) ,tmp_it++, it--;
-
-    //print();
-    
-    for (auto it = heads[mp[c]].begin(); it != heads[mp[c]].end(); it++)
-        if (*it == c)
-            heads[mp[c]].insert(it, all(tmp));
-       
+    // 출력
+    for (int i = 1; i <= m; i++) printLine(i);
 }
