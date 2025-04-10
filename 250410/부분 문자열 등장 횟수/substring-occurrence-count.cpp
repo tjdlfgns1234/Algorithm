4d72bae19f62f2ff9f5f0c6100ef97da7f2b7828
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2025-04-10 09:54:31 +0900

    Title: 부분 문자열 등장 횟수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/부분 문자열 등장 횟수/substring-occurrence-count.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define K 256
+#define MOD 127
+#define MAX 100001
+#define INF 987654321
+#define ll long long
+using namespace std;
+
+int n, k;
+string a, b;
+int f[MAX] = { 0 };
+
+
+void solve();
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(), cout.tie();
+
+	solve();
+
+	return 0;
+}
+void solve() {
+	cin >> a >> b;
+
+	int n = a.size();
+	int m = b.size();
+
+	a = "#" + a;
+	b = "#" + b;
+
+	f[0] = -1;
+	
+
+	for (int i = 1; i <= m; i++) {
+		int j = f[i - 1];
+
+		while (j >= 0 && b[j + 1] != b[i])
+			j = f[j];
+		f[i] = j + 1;
+	}
+
+	int cnt = 0;
+	int j = 0;
+	for (int i = 1; i <= n; i++) {
+		while (j >= 0 && b[j + 1] != a[i])
+			j = f[j];
+
+		j++;
+
+		if (j == m) {
+			// cout << i << '\n';
+			cnt++;
+			j = f[j];
+		}
+	}
+	cout << cnt;
+}
