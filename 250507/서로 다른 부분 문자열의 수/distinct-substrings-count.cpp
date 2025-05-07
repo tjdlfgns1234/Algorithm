554fa27ebb98260e8997c32a81d37cecced5850b
tjdlfgns1234 <58213713+tjdlfgns1234@users.noreply.github.com>
2025-05-07 15:13:44 +0900

    Title: 서로 다른 부분 문자열의 수, Time: 1883ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250507/서로 다른 부분 문자열의 수/distinct-substrings-count.cpp
+++ 250507/서로 다른 부분 문자열의 수/distinct-substrings-count.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 #define all(a) a.begin(),a.end()
 #define rall(a) a.rbegin(),a.rend()
-#define k 347
+#define k 31
 #define MOD 1000000007
+#define MAX 5001
 #define INF 987654321
 #define ll long long
 using namespace std;
 
 string st;
-int ans = 0;
-ll arr[5'001] = { 0 };
+ll arr[MAX] = { 0 };
 void solve();
 
 int main() {
@@ -22,21 +22,37 @@ int main() {
 	return 0;
 }
 void solve() {
-    cin >> st;
+	cin >> st;
+	ll ans = 0;
+	ll key = 0;
 
+	arr[0] = 1;
 
-    int n = st.size();
+	int n = st.size();
+	for (int i = 1; i <= MAX; i++)
+		arr[i] = (arr[i - 1] * k) % MOD;
 
-    for(int i = 0; i < n;i++){
-        set<string> s;
-        for(int j = 0; j < n-i;j++){
-         
-            s.insert(st.substr(j, i + 1));
-            //ans += s.size();
-            //s.clear();
-        }
-        ans += s.size();
-    }
-    // ans = s.size();
-    cout << ans;
+	ll hash = 0;
+
+	// 일단 부분 문자열 부터 만들자.
+	for(int size = 1; size <= n;size++){
+		set<ll> s;
+		hash = 0;
+		for (int i = 1; i <= size; i++) {
+			hash += st[i-1]*arr[size-i];
+			hash %= MOD;
+		}
+		s.insert(hash);
+
+		for (int i = size; i < n; i++) {
+			hash = ((hash - st[i-size] * arr[size-1] )% MOD + MOD) % MOD;
+			hash = (hash * k) % MOD;
+			hash = (hash + st[i]) % MOD;
+			s.insert(hash);
+			// cout << hash << '\n';
+		}
+		// cout << s.size() << " " << ans << '\n';
+		ans += s.size();
+	}
+	cout << ans;
 }
